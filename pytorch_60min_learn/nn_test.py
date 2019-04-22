475565ed2adc48fee2083dd03dfcfb5ebc16e70d
smt <735490850@qq.com>
2019-04-22 19:21:41 +0800

    add pytorch test

--- pytorch_60min_learn/nn_test.py
+++ pytorch_60min_learn/nn_test.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+import torch.nn as nn  # 貌似有参数的都在nn里面，没有参数的都在functional里面
 import torch.nn.functional as F
 import torch.optim as optim
 
@@ -16,6 +16,7 @@ class Net(nn.Module):  # 将Net作为nn.Module的子类
 
     # 定义前向传播的过程，这个是必须的，一旦定义成功，反向传播的过程是自动求导实现的
     def forward(self, x):
+        # 构建计算图的过程是在前向计算中完成的 所谓的动态图结构
         x = F.max_pool2d(self.conv1(x), (2, 2))  # 使用2x2的窗口大小进行最大池化操作，更新x
         x = F.max_pool2d(self.conv2(x), 2)  # 如果窗口大小是一个正方形的，就可以只指定一个数字
         x = x.view(-1, self.flatten(x))  # 拉直，通过flatten函数可以得到特征的数目，-1表示不管有多少行，出来的就是一个batch有几个图片
@@ -35,7 +36,7 @@ class Net(nn.Module):  # 将Net作为nn.Module的子类
 net = Net()  # 实例化一个Net
 print(net)  # 查看网络结构
 
-params = list(net.parameters())
+params = list(net.parameters()) # 模型中可学习的参数会由net.parameters()返回
 print(len(params))  # 这里是10，因为有两个卷积层三个全连接层，每层都有W和bias，所以一共十个
 print(params[0].size())  # 就是第一个卷积层的参数
 
@@ -46,8 +47,8 @@ print("\noutput:", output)
 
 # 在反向传播之前需要将所有参数的梯度缓存清零 否则是不断累加的
 # net.zero_grad()
-# output.backward(torch.randn(1, 10))  # 这里不明白 为什么要传一个张量进去
-# torch.nn只支持小批量的输入，如果只输入单个样本，需要使用input.unsqueeze(0)，来添加它的维数
+# output.backward(torch.randn(1, 10))
+# torch.nn只支持小批量的输入，例如，nn.Conv2d能够结构一个四维的TensornSamples x nChannels x Height x Width,如果只输入单个样本，需要使用input.unsqueeze(0)，来添加它的维数
 
 # 接下来开始损失函数和更新权值
 # 损失函数接收一对output和target，target就是真实标签值
@@ -55,7 +56,7 @@ target = torch.randn(1, 10)  # 自己设置的真实标签值
 criterion = nn.MSELoss()  # 选择一个均方误差类
 loss = criterion(output, target)
 print("\nloss:", loss)
-print(loss.grad_fn)
+print("\nloss.grad_fn:", loss.grad_fn)
 print(loss.grad_fn.next_functions[0][0])  # 反向跟踪loss 它的loss前一步是linear
 
 # 开始反向传播
@@ -74,5 +75,6 @@ for i in range(1000):
     loss = criterion(output, target)
     loss.backward()
     optimizer.step()  # 更新
+print(loss)
 
 
